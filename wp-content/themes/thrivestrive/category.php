cfe2125d2dc82f5b58adca3edf5320833fb222ea
Scrivs <scrivs@gmail.com>
2016-05-17 16:18:47 -0600

    add category page

---
+++ wp-content/themes/thrivestrive/category.php
@@ -0,0 +1,19 @@
+<?php get_header();?>
+<?php if ( have_posts() ) : ?>
+<section class="hero">
+	<div class="row">
+		<div class="small-12 columns">
+			<h1><?php printf( single_cat_title( '', false ) ); ?></h1>
+		</div>
+	</div>
+</section>
+<section class="article">
+	<?php while ( have_posts() ) : the_post(); ?>
+		<div class="article-box">
+			<h2 class="text-center"><a href="<?php echo the_permalink();?>"><?php echo the_title(); ?></a></h2>
+			<?php the_excerpt(); ?>
+		</div>
+	<?php endwhile; ?>
+</section>
+<?php endif; ?>
+<?php get_footer();?>
