e00ff94838a78c58a7696084b46ad04dd28fc97c
Stephen <stephen@Stephens-MacBook-Pro.local>
2016-05-17 11:14:01 -0600

    test git hub intergration

---
+++ wp-content/themes/thrivestrive/single.php
@@ -0,0 +1,97 @@
+<?php
+get_header();
+?>
+	<div class="row">
+		<div class="small-12 columns text-center">
+			<img src="http://placehold.it/850x150">
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="small-12 columns">
+			<h1>Article Title</h1>
+			<img src="https://static.pexels.com/photos/39688/training-train-lime-barbell-39688-medium.jpeg" alt="Weights" />
+			<p>I'm not the biggest guy or the strongest, nor do I really want to be that person. I just want to be the guy that is happy with waking up and not feeling like a complete slob. Going to the gym does this for me. Are there days where I really don't want to get up, change, walk to the gym and workout? Shit, I'd be lying if I said I didn't get that feeling about four days a week. The one thing I know after each workout though, as shitty and tired as I feel, I have a sense of accomplishment about myself. I know that I have done something that has made me better for not only tomorrow, but for the future me as well. Trust me, during any workout I'm pissed and ask myself why I'm doing this to myself when I can be getting an extra hour of sleep. It all feels worth it though when the workout is completed. Plus I know that I will more than likely look that much better when I'm naked.</p>
+
+			<p>I've had a few stints where I would go to the gym religiously everyday and workout. The latest one started a little over a month ago with my sister-in-law and brother. Safe to say we all were a little on the lazy side when it came to having a regular workout routine. We all want to be fit and we know the best way to do this is to push each other as a family. If one of us has to suffer through the pain and struggle of a workout, all of us are going to do the same. We stick together and will honestly do anything for each other. So when we set goals for ourselves, its not just an individual effort, it is the effort of the entire family that helps us reach our goal. Our expectations are high, always have been, and we know what each other is capable of. So if one of us doesn't perform up to a certain standard we all know they are capable of, we get on them and force them to reach the level they should be at.</p>
+
+			<h3>Welcome</h3>
+
+			<p>Making the decision to start is the second most difficult thing to me when it comes to working out and since you're reading this, I assume that you have already made the decision to want to start. Now its time to start putting that thought into action. Congratulations, that's a good first step. There's a lot people out there that don't ever make a decision like this. Welcome to the first step to a better you.</p>
+
+			<h3>Goals</h3>
+
+			<p>Before you even start to go to the gym, you should set yourself some goals.  Set both short-term goals and long-term goals. Make sure, and I cannot stress this enough, that these goals are realistic. I'm an optimist and all for reaching for the stars but unless you plan on becoming a power lifter, make these goals attainable. For example, you do not want to set a goal saying that you will bench 300lbs after the first two weeks. That is just nearly impossible. Set a goal that says you will perform your workout routine for 14 straight days without a rest day. That is a good short-term goal. Long-term should be something that says you will curl at least twice as much weight as you started with after 3 months of continuously working out. Goals are meant to be set to help you reach for something. Keep your goals inside the atmosphere, don't put them in space quite yet.</p>
+
+			<h3>Future You</h3>
+			<img src="https://static.pexels.com/photos/54377/pexels-photo-54377-large.jpeg" alt="Mirror" />
+			<p>Picture how you want to look and picture how you are going to get there. Envision yourself a month from now, three months from now, a year from now. While you are doing this, try to remember one thing, this decision you are making should be for you. Nobody else. It's okay to have other people in mind, but ultimately do it for yourself. Knowing that you are doing this for yourself will only motivate you that much more because lets be honest, sometimes other people just get in the way of who we want to be. Okay, you see the future you? Good, now tell them to take a seat and that you'll see them in no time.</p>
+
+			<h3>Excuses?!</h3>
+
+			<p>The next beneficial thing you need to do is to think of every possible excuse you can think of and throw it out the window. Nothing is going to be accomplished with excuses. This should just be a general rule for anybody in life, not just when it comes to your personal health and fitness. Excuses hold everyone back when they make them. Hold yourself accountable for all of your actions. If there is no accountability, you're just giving yourself an easy out. Don't be that person that thinks it's okay to ease through things and not fully accomplish your goals. You set those goals with high expectations, I hope, so you might as well stick to those expectations and do what you need to do. One of my favorite quotes from a great movie, <em>Wolf of Wall Street</em>, "The only thing standing between you and your goal is the bullshit story you keep telling yourself as to why you can't achieve it." Excuses get you nowhere so get rid of them now.
+
+			<h3>It will not be easy</h3>
+
+			<p>Getting started on a regular workout routine is always the hardest part in my opinion. Trust me, I understand the struggle its gonna be at first. When you go from being lazy and sleeping in almost everyday to waking up for the gym and starting a workout at 8am, the shock to your body can be pretty extreme. Me being the person that I am, I still thought that I could lift the same weights I could when I was in college. I was able to lift the same amount of weight, but to say I was struggling would be a major understatement. Then the next day, I don't think I was able to move, but since we all had literally just started going to the gym again, I did not want to disappoint anybody so I cried myself through the rest of the first week of workouts.</p>
+
+			<h3>Don't Lift Everything</h3>
+
+			<p>When you're just starting to workout in the gym, you do not want to go crazy and try to lift as much weight as you think you can. It's definitely the smart move to ease your way into the routine. I'm not saying lift the lightest weights possible but I am saying that you should not try to go world's strongest man and try to lift more than everyone else. What good would that do you anyway? You'll just be too sore the next couple days and probably skip going to the gym anyway and your routine is already screwed up. Next thing you know you're having to start from step one all over again. Then the dreaded process begins again.</p>
+
+			<p>Lift too much, get too sore, rest the next few days by not going to the gym, fall back into your lazy ways, start working out again and the cycle repeats and continues to repeat. I know people that fall into this cycle and its honestly pretty comical. We all know the definition of insanity, trying the same thing over and over again and expecting different results.</p>
+
+			<p>Take the first few days, maybe even the first week, to ease your body into the flow of the workouts. Mentally you might be ready but more than likely, your body is not. DO NOT GO CRAZY! You will get sore, but that gets much easier to deal with and is less frequent the more you stick to the gym. Think of it like this, if you're in pain don't quit, you're already in pain so you might as well get a reward for it. Reward yourself with hard work and the prizes are much better than a stuffed animal you get from the fair.</p>
+
+			<h3>Routine</h3>
+
+			<p>Something that is vital to anybody that is just beginning at the gym is to establish some sort of routine. Plan out your workouts and make sure you get to the gym everyday at around the same time. This just makes everything easier. You do not want to be going to the gym before lunch one day, then going after dinner the next day. It is nice to keep your body guessing, but it is easier to make it to the gym at the same time every single day. This keeps you motivated and accountable. It takes a while to form a habit and you do not want to be the person that keeps breaking a possible healthy habit because they can't stick to a schedule. A great quote, that I have heard multiple times, that is a good motivator when it comes to sticking to a routine, "Are the habits you have today on par with the dreams you have for tomorrow." The dream is what you want yourself to be and the habits are your routine. Once you have your routine, don't let it falter, don't break it. </p>
+
+			<h3>Explore</h3>
+
+			<p>If you've never really been the type to workout and are planning on beginning a new chapter in your life, it is hard to determine what type of workouts you should do or what you even enjoy doing. My advice, the first couple weeks, just try different types of workouts. The only way to find out exactly what you enjoy doing is to actually try it. Working out is way more fun when you actually enjoy what you're doing. There have been plenty of times where a workout just seems to drag on and on just because I didn't like what we were doing that day. Some workouts for certain body parts may feel awkward to you but others for the same body part may feel perfect. It is important that you do try as many new workouts as you can. Keeping your body guessing in this way is great and you will see a change in yourself much faster, in my unprofessional professional opinion. If you find some exercises that you love to do and do not want to change from that specific exercise then don't. Doing anything is beneficial. Just make sure you find a way to enjoy the workouts that you are doing and everything else will come easier.</p>
+
+			<h3>Technique,  technique, technique</h3>
+
+			<p>Proper technique may be the most important aspect of working out when you are just beginning. Learning proper technique for exercises not only ensures that you get the most out of your workout, but it also ensures that you are keeping yourself as safe as possible. The gym can be a dangerous place and injuries happen ALL the time. We all know what an injury does to us, it sets us back and we have to put many of our goals on hold. Then when we finally get back from the injury, it will feel as though we are almost starting over and playing a game of catch up with the person we wanted to be. Nobody wants a setback like this. Also, what's the point in working out if you aren't going to maximize your time and effort? You are wanting to go to the gym for a reason, right? You want to start a new chapter in your life? Learn the proper technique for all the exercises you want to do. It may take a little bit to perfect these techniques but they are there for a reason. If you aren't sure of the proper technique right away, go ahead and ask somebody or check out youtube. We all know youtube and google have all the answers to life so you might as well utilize them.</p>
+
+			<h3>What to wear?</h3>
+			<img src="https://static.pexels.com/photos/29353/pexels-photo-29353-medium.jpg" alt="Clothing" />
+
+			<p>One thing that is equally as important as everything else mentioned earlier but probably overlooked is gym attire. Yes, proper gym attire is a thing. Showing up to the gym without proper gym attire not only looks awkward but can be very dangerous, not only for you but for everyone around you as well. What is proper gym attire? Its a simple answer, t-shirt, shorts/pants, and tennis shoes. You do not want anything that is too loose or baggy because these could get caught on weights. Tennis shoes is something that is mainly for your own comfort. Any kind of shoe could work for you, but do not wear sandals. I have never seen anything good come from somebody working out in sandals. It's like the gym gods know and something always happens to the shoeless wonder. My personal preference of shoe is anything Nike. I've always been a fan of Nike running shoes and that's what has always worked best for me in the gym. They provide the most comfort while giving me some stability as well when I get a little out there with the weights.</p>
+
+			<h3>Push yourself</h3>
+
+			<p>Know that this is not going to be a cake walk (if it is I'm going to assume that you just didn't push yourself at all). This is going to be tough. There are going to be times where you just want to take a break from it all. There will be days where you have a lack of motivation to do anything. It happens to everyone. Remember that it is crucial to keep your goals in mind and continue to push yourself under any circumstance. The more you push, the better results you will get, the better you will feel about yourself, and the future you that you saw earlier will be on your doorstep in no time.</p>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="small-12 columns text-center">
+			<form class="callout">
+				<label>
+					Want to learn the basics to gym equipment? Subscribe now for a free guide
+				</label>
+				<input type="text" name="email">
+				</input>
+				<button type="Submit" class="large button">Submit</button>
+			</form>
+		</div>
+	</div>
+
+	<footer>
+		<div class="expanded row large-collapse">
+			<div class="small-4 columns">
+				<img src="http://placehold.it/550x150">
+			</div>
+			<div class="small-4 columns">
+				<img src="http://placehold.it/550x150">
+			</div>
+			<div class="small-4 columns">
+				<img src="http://placehold.it/550x150">
+			</div>
+		</div>
+	</footer>
+<?php
+get_footer();
+?>
