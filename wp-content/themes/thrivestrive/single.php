3db8c47168dfbf5bc1f1f1b05541626d486f4353
Scrivs <scrivs@gmail.com>
2016-05-17 16:02:09 -0600

    adjust single page style

--- wp-content/themes/thrivestrive/single.php
+++ wp-content/themes/thrivestrive/single.php
@@ -5,45 +5,30 @@ get_header();
 <section class="hero">
 	<div class="row">
 		<div class="small-12 columns text-center">
-			<img src="http://placehold.it/850x150">
+			<?php the_title( '<h1 class="entry-title text-center">', '</h1>' ); ?>
 		</div>
 	</div>
 </section>
 <section class="article">
 	<div class="row">
 		<div class="small-12 large-6 large-offset-3 columns">
-			<?php the_title( '<h1 class="entry-title text-center">', '</h1>' ); ?>
 			<?php the_content('Read more...'); ?>
 		</div>
 	</div>
 </section>
 <?php endwhile;endif; ?>
+<section class="profiles">
 	<div class="row">
 		<div class="small-12 large-6 columns large-offset-3 text-center">
 			<form class="callout">
-				<label for="email">
-					Want to learn the basics to gym equipment? Subscribe now for a free guide
+				<label for="email">Want to learn the basics to gym equipment? Subscribe now for a free guide
+					<input type="email" name="email" placeholder="you.are@awesome.com">
 				</label>
-				<input type="email" name="email" placeholder="you.are@awesome.com">
-				</input>
-				<button type="Submit" class="button">Get the guide</button>
+				<button type="Submit" class="button expanded">Get the guide</button>
 			</form>
 		</div>
 	</div>
-
-	<footer>
-		<div class="expanded row large-collapse">
-			<div class="small-4 columns">
-				<img src="http://placehold.it/550x150">
-			</div>
-			<div class="small-4 columns">
-				<img src="http://placehold.it/550x150">
-			</div>
-			<div class="small-4 columns">
-				<img src="http://placehold.it/550x150">
-			</div>
-		</div>
-	</footer>
+</section>
 <?php
 get_footer();
 ?>
