19f4d54b6f6fb39ede83e485620be24dea46a0e6
Scrivs <scrivs@gmail.com>
2016-05-17 10:23:53 -0600

    init

--- wp-content/themes/thrivestrive/header.php
+++ wp-content/themes/thrivestrive/header.php
@@ -5,7 +5,6 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="profile" href="http://gmpg.org/xfn/11">
-		<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/app.css" media="screen" type="text/css" />
 		<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 		<?php endif; ?>
