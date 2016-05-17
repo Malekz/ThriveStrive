2b45728b27ec01e3245fc263568ce4f0b239b0f4
Scrivs <scrivs@gmail.com>
2016-05-17 16:08:21 -0600

    adjust cta

--- wp-content/themes/thrivestrive/single.php
+++ wp-content/themes/thrivestrive/single.php
@@ -19,9 +19,12 @@ get_header();
 <?php endwhile;endif; ?>
 <section class="profiles">
 	<div class="row">
-		<div class="small-12 large-6 columns large-offset-3 text-center">
+		<div class="small-12 large-8 large-offset-2 end columns">
+			<h3 class="text-center">Want to learn the basics to gym equipment? Subscribe now for a free guide.</h3>
+		</div>
+		<div class="small-12 large-6 columns large-offset-3 text-center end">
 			<form class="callout">
-				<label for="email">Want to learn the basics to gym equipment? Subscribe now for a free guide
+				<label for="email">
 					<input type="email" name="email" placeholder="you.are@awesome.com">
 				</label>
 				<button type="Submit" class="button expanded">Get the guide</button>
