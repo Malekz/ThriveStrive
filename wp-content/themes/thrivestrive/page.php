e9b453720084776ee959087a9662880f73ad4753
Scrivs <scrivs@gmail.com>
2016-05-17 19:57:55 -0600

    remove columns from page template

--- wp-content/themes/thrivestrive/page.php
+++ wp-content/themes/thrivestrive/page.php
@@ -2,7 +2,7 @@
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 <section class="article">
 	<div class="row">
-		<div class="small-12 large-6 large-offset-3 columns">
+		<div class="small-12 columns">
 			<?php the_title( '<h1 class="entry-title text-center">', '</h1>' ); ?>
 			<?php the_content(); ?>
 		</div>
