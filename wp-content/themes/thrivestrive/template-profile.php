5f129cab4950b9d20986648547166ae3694a36fd
Scrivs <scrivs@gmail.com>
2016-05-17 20:01:24 -0600

    add profile template

--- wp-content/themes/thrivestrive/template-profile.php
+++ wp-content/themes/thrivestrive/template-profile.php
@@ -1,5 +1,18 @@
+<?php
+/*
+Template Name: Profile
+*/
+?>
+
 <?php get_header(); ?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<section class="hero">
+	<div class="row">
+		<div class="small-12 columns">
+			<?php the_title( '<h1 class="entry-title text-center">', '</h1>' ); ?>
+		</div>
+	</div>
+</section>
 <section class="article">
 	<div class="row">
 		<div class="small-12 large-6 large-offset-3 columns">
