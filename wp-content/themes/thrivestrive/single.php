464efc33d0752593e86c5a51ae66fa3a99abbcb1
Scrivs <scrivs@gmail.com>
2016-05-19 03:24:35 -0600

    add custom hero and pinterest images to post page

--- wp-content/themes/thrivestrive/single.php
+++ wp-content/themes/thrivestrive/single.php
@@ -3,24 +3,28 @@ get_header();
 ?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 <?php 
-$categories = get_the_category();
- 
-if ( ! empty( $categories ) ) {
-    $category = strtolower(esc_html( $categories[0]->name ));   
-}
+	$image = get_field('hero_image');
+	$subtitle = get_field('subtitle');
+	$pinterest = get_field('pinterest');
 ?>
-<section class="hero <?php echo $category; ?>">
+<section class="hero" style="<?php if($image != '') { ?> background: linear-gradient(90deg, rgba(255,104,71, 0.5) 10%, rgba(132,220,198, 0.5) 90%), url('<?php echo $image['url']; ?>') no-repeat fixed center;background-size: cover;" <?php } ?>">
 	<div class="row">
-		<div class="small-12 columns text-center">
+		<div class="small-12 columns">
 			<?php the_title( '<h1 class="entry-title text-center">', '</h1>' ); ?>
+			<?php if($subtitle != '') echo '<h2 class="subheader">'.$subtitle.'</h2>'; ?>
 		</div>
 	</div>
 </section>
 <section class="article">
 	<div class="row">
-		<div class="small-12 columns">
-			<?php the_content('Read more...'); ?>
+		<div class="small-12 <?php if($pinterest != '') echo 'large-7'; ?> columns">
+			<?php the_content(); ?>
 		</div>
+		<?php if($pinterest != '') { ?>
+			<div class="small-12 large-5 columns">
+				<div class="image"><img src="<?php echo $pinterest;?>"></div>
+			</div>
+		<?php } ?>
 	</div>
 </section>
 <?php endwhile;endif; ?>
