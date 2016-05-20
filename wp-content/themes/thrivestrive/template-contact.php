5f890cc0957b8cdef99d9f0484f38799e628454d
Stephen <scrivsjr@yahoo.com>
2016-05-20 17:44:09 -0600

    Change social media columns

---
+++ wp-content/themes/thrivestrive/template-contact.php
@@ -0,0 +1,95 @@
+<?php
+/*
+Template Name: Contact
+*/
+?>
+
+<?php get_header(); ?>
+	<div class="hero">
+		<h1>Contact Us</h1>
+	</div>
+
+	<div class="article text-center">
+		<p>If you have any stories you would like to share or any questions about anything from our site, don't hesistate to email us at: <strong>hello@thrivestrive.com</strong></p>
+	</div>
+
+
+
+	<section class="sections">
+		<div class="row">
+			<div class="small-12 columns">
+				<h2 class="text-center">Be sure to follow our journey</h2>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="small-12 large-6 columns">
+				<div class="section-box">
+					<h3 class="text-center"><a href="https://twitter.com/thrivesite">Twitter</a></h3>
+				</div>
+			</div>
+			<div class="small-12 large-6 columns">
+				<div class="section-box">
+					<h3 class="text-center"><a href="http://www.facebook.com/ThriveStrive">Facebook</a></h3>
+				</div>
+			</div>
+			<div class="small-12 large-6 columns">
+				<div class="section-box">
+					<h3 class="text-center"><a href="">Pinterest</a></h3>
+				</div>
+			</div>
+			<div class="small-12 large-6 columns">
+				<div class="section-box">
+					<h3 class="text-center"><a href="">Instagram</a></h3>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="sections social">
+		<div class="row">
+			<div class="small-12 columns">
+				<h2 class="text-center">You can also follow each of us individually if you like, we are just regular people</h2>
+			</div>
+		
+
+		
+			<div class="small-12 medium-6 medium-offset-3 large-4 columns text-center">
+				<div class="callout">
+					<h2>Elisa</h2>
+						<div class="section-box">
+							<h3 class="text-center"><a href="">Instagram</a></h3>
+						</div>
+						<div class="section-box">
+							<h3 class="text-center"><a href="">Pinterest</a></h3>
+						</div>
+				</div>
+			</div>
+
+			<div class="small-12 medium-6 medium-offset-3 large-4 columns text-center">
+				<div class="callout">
+					<h2>Paul</h2>
+						<div class="section-box">
+							<h3 class="text-center"><a href="">Twitter</a></h3>
+						</div>
+						<div class="section-box">
+							<h3 class="text-center"><a href="">Instagram</a></h3>
+						</div>
+				</div>
+			</div>
+
+			<div class="small-12 medium-6 medium-offset-3 large-4 columns text-center">
+				<div class="callout">
+					<h2>Stephen</h2>
+					<div class="section-box">
+						<h3 class="text-center"><a href="">Instagram</a></h3>
+					</div>
+					<div class="section-box">
+						<h3 class="text-center"><a href="">Twitter</a></h3>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+<?php get_footer(); ?>
