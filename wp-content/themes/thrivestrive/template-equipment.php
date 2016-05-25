f83535a63acfadb2a394b6f788d863167cb8eb16
Stephen <scrivsjr@yahoo.com>
2016-05-25 11:18:56 -0600

    Change ebook landing page

---
+++ wp-content/themes/thrivestrive/template-equipment.php
@@ -0,0 +1,45 @@
+<?php
+/*
+Template Name: E-book Equipment
+*/
+?>
+
+<?php get_header(); ?>
+	<section class="hero">
+		<div class="row">
+			<div class="small-12 columns">
+				<h2>Guide to Gym Equipment</h2>
+			</div>
+		</div>
+	</section>
+
+	<section class="article">
+		<div class="row">
+			<div class="small-12 large-centered columns">
+				<img src="">Photo of book cover
+			</div>
+		</div>
+	</section>
+
+	<section class="article">
+		<div class="row">
+			<div class="small-12 large-6 columns">
+				<p>Are you just beginning at the gym?</p>
+				<p>Are you ready to start a new chapter in your life?</p>
+				<p>Do you need to learn how to use everything you may see in the gym?</p>
+				<p>Do you even know what you should expect to see when you walk in the gym?</p>
+				<p>Do you know how to properly use each piece of gym equipment?</p>
+			</div>
+
+			<div class="small-12 large-6 columns text-center">
+				<form class="callout">
+					<label>
+						<p>Sign up now to receive the Guide to Gym Equipment.</p>
+						<p>We are normal people here at ThriveStrive and can relate to you!</p>
+						<input class="text-center" type="text" name="email" value="Email Address"></input>	
+					</label>
+						<button class="large button" type="submit">Subscribe</button>
+				</form>
+	</section>
+
+<?php get_footer(); ?>
