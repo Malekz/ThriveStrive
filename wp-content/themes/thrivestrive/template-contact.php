bdd2a09d6cf84e810a5b2ca4c9bcf53e28495125
Scrivs <scrivs@gmail.com>
2016-06-01 14:50:45 -0600

    add cta to profile pages

--- wp-content/themes/thrivestrive/template-contact.php
+++ wp-content/themes/thrivestrive/template-contact.php
@@ -101,5 +101,45 @@ Template Name: Contact
 			</div>
 		</div>
 	</section>
+<section class="cta slate">
+	<div class="row">
+		<div class="small-12 columns">
+			<h3 class="text-center subheader">Getting Ready to Start a Fitness Journey? Make Sure Your Mind Is Just as Ready as Your Body With Our</h3>
+			<h2 class="text-center"><em>Free 5-Day Mind Over Matter Email Course</em></h2>
+			<script src="https://app.convertkit.com/assets/CKJS4.js?v=21"></script>
+			<div class="ck_form_container ck_inline" data-ck-version="5">
+			  <div class="ck_form ck_naked">
+			  <div class="ck_form_fields">
+			    <div id="ck_success_msg" style="display:none;">
+			      <p>Success! Now check your email to confirm your subscription.</p>
+			    </div>
 
+			    <!--  Form starts here  -->
+			    <form id="ck_subscribe_form" class="ck_subscribe_form callout" action="https://app.convertkit.com/landing_pages/60579/subscribe" data-remote="true">
+			      <input type="hidden" value="{&quot;form_style&quot;:&quot;naked&quot;,&quot;embed_style&quot;:&quot;inline&quot;,&quot;embed_trigger&quot;:&quot;scroll_percentage&quot;,&quot;scroll_percentage&quot;:&quot;70&quot;,&quot;delay_seconds&quot;:&quot;10&quot;,&quot;display_position&quot;:&quot;br&quot;,&quot;display_devices&quot;:&quot;all&quot;,&quot;days_no_show&quot;:&quot;15&quot;,&quot;converted_behavior&quot;:&quot;show&quot;}" id="ck_form_options">
+			      <input type="hidden" name="id" value="60579" id="landing_page_id">
+			      <div class="ck_errorArea">
+			        <div id="ck_error_msg" style="display:none">
+			          <p>There was an error submitting your subscription. Please try again.</p>
+			        </div>
+			      </div>
+			      <div class="ck_control_group ck_email_field_group">
+			        <label class="ck_label" for="ck_emailField" style="display: none">Email Address</label>
+			        <input type="text" name="first_name" class="ck_first_name" id="ck_firstNameField" placeholder="First Name">
+			        <input type="email" name="email" class="ck_email_address" id="ck_emailField" placeholder="Email Address" required>
+			      </div>
+
+
+			      <button class="subscribe_button ck_subscribe_button expanded button btn fields" id="ck_subscribe_button">
+			        I&#x27;m Ready to Start!
+			      </button>
+			    </form>
+			  </div>
+
+			</div>
+
+			</div>
+		</div>
+	</div>
+</section>
 <?php get_footer(); ?>
