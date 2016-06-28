9841a17e1049cddba7c1c5cfd3fc583bb70c5e45
Stephen <scrivsjr@yahoo.com>
2016-06-28 13:37:24 -0600

    change muscle template

--- wp-content/themes/thrivestrive/template-muscles.php
+++ wp-content/themes/thrivestrive/template-muscles.php
@@ -6,4 +6,93 @@ Template Name: Muscles
 
 <?php get_header(); ?>
 
+<section class="hero">
+	<div class="row">
+		<div class="title small-12 columns">
+			<h1>Muscles</h1>
+				<h2 class="subheader">Find an exercise for every muscle you want to work</h2>
+		</div>
+	</div>
+</section>
+
+<section class="muscles">
+	<div class="row">
+		<div class="small-12 large-4 columns text-center">
+			<div class="chest">
+				<h3>Chest</h3>
+				<p><a href="">Upper Chest</a></p>
+				<p><a href="">Lower Chest</a></p>
+				<p><a href="">Entire Chest</a></p>
+			</div>
+		</div>
+
+		<div class="small-12 large-4 columns text-center">
+			<div class="legs">
+				<h3>Legs</h3>
+				<p><a href="">Hamstrings</a></p>
+				<p><a href="">Quadriceps</a></p>
+				<p><a href="">Calves</a></p>
+				<p><a href="">Butt</a></p>
+			</div>
+		</div>
+
+		<div class="small-12 large-4 columns text-center">
+			<div class="shoulders">
+				<h3>Shoulders</h3>
+				<p><a href="">Front Side</a></p>
+				<p><a href="">Back Side</a></p>
+				<p><a href="">Entire Shoulder</a></p>
+			</div>
+		</div>
+
+	</div>
+
+
+
+	<div class="row">
+
+		<div class="small-12 large-4 columns text-center">
+			<div class="back">
+				<h3>Back</h3>
+				<p><a href="">Upper Back</a></p>
+				<p><a href="">Lower Back</a></p>
+			</div>
+		</div>
+
+		<div class="small-12 large-4 columns text-center">
+			<div class="abs">
+				<h3>Abs</h3>
+				<p><a href="">Upper Abs</a></p>
+				<p><a href="">Lower Abs</a></p>
+				<p><a href="">Obliques</a></p>
+				<p><a href="">Entire Abs</a></p>
+			</div>
+		</div>
+		
+		<div class="small-12 large-4 columns text-center">
+			<div class="arms">
+				<h3>Arms</h3>
+				<p><a href="">Forearms</a></p>
+				<p><a href="">Back of the Forearms</a></p>
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="small-12 large-4 columns text-center">
+			<div class="biceps">
+				<h3>Biceps</h3>
+				<p><a href="">Entire Bicep</a></p>
+			</div>
+		</div>
+
+		<div class="small-12 large-4 columns text-center">
+			<div class="triceps">
+				<h3>Triceps</h3>
+				<p><a href="">Entire Tricep</a></p>
+			</div>
+		</div>
+	</div>
+
+</section>
 <?php get_footer(); ?>
