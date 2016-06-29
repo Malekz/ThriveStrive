08899eeba170dea0f2a472c05339cb4175d142da
Scrivs <scrivs@gmail.com>
2016-06-29 14:54:05 -0600

    style exercise page

--- wp-content/themes/thrivestrive/template-exercises.php
+++ wp-content/themes/thrivestrive/template-exercises.php
@@ -5,27 +5,39 @@ Template Name: Exercises
 ?>
 
 <?php get_header(); ?>
-<section>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<section class="hero">
 	<div class="row">
-		<div class="exercise small-12 large-8 columns text-center">
-		<?php the_content(); ?>
+		<div class="small-12 columns">
+			<div class="title-box">
+				<?php the_title( '<h1 class="entry-title text-center">', '</h1>' ); ?>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="article exercises">
+	<div class="row">
+		<div class="exercise small-12 large-8 columns">
+			<?php the_content(); ?>
 		</div>
 
-		<div class="small-12 large-4 columns text-center">
+		<div class="small-12 large-4 columns sidebar">
 			<div class="body">
-				<h2>Body</h2>
-				<h3><a href="">Chest</a></h3>
-				<h3><a href="">Legs</a></h3>
-				<h3><a href="">Shoulders</a></h3>
-				<h3><a href="">Back</a></h3>
-				<h3><a href="">Abs</a></h3>
-				<h3><a href="">Arms</a></h3>
-				<h3><a href="">Biceps</a></h3>
-				<h3><a href="">Triceps</a></h3>
+				<h3>Muscle Groups</h3>
+				<ul class="no-bullet">
+					<li><a href="">Chest</a></li>
+					<li><a href="">Legs</a></li>
+					<li><a href="">Shoulders</a></li>
+					<li><a href="">Back</a></li>
+					<li><a href="">Abs</a></li>
+					<li><a href="">Arms</a></li>
+					<li><a href="">Biceps</a></li>
+					<li><a href="">Triceps</a></li>
+				</ul>
 			</div>
 		</div>
 	</div>
 </section>
 
-
+<?php endwhile; endif; ?>
 <?php get_footer(); ?>
