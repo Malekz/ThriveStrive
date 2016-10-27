86178ff20de174f12d7b1bef0a561878123e8851
Scrivs <scrivs@gmail.com>
2016-10-27 09:30:07 -0700

    init

--- wp-content/themes/thrivestrive/template-muscles.php
+++ wp-content/themes/thrivestrive/template-muscles.php
@@ -8,86 +8,94 @@ Template Name: Muscles
 
 <section class="hero">
 	<div class="row">
-		<div class="title small-12 columns">
-			<h1>Muscles Specific Exercises</h1>
-			<h2 class="subheader">Find an exercise for every muscle you want to work</h2>
+		<div class="small-12 columns">
+			<h1>Muscle Specific Exercises</h1>
+			<h2 class="subheader">Find an Exercise for Every Muscle You Want to Work</h2>
 		</div>
 	</div>
 </section>
 
 <section class="muscles">
-	<div class="row">
-		<div class="small-12 large-4 columns text-center">
-			<div class="group">
+	<div class="row" data-equalizer>
+		<div class="small-12 large-4 columns">
+			<div class="group" data-equalizer-watch>
 				<h3><a href="/chest">Chest</a></h3>
-				<p><a href="/chest/#upper-chest">Upper Chest</a></p>
-				<p><a href="/chest/#lower-chest">Lower Chest</a></p>
-				<p><a href="/chest/#entire-chest">Entire Chest</a></p>
+				<ul>
+					<li><a href="/chest/#upper-chest">Upper Chest</a></li>
+					<li><a href="/chest/#lower-chest">Lower Chest</a></li>
+					<li><a href="/chest/#entire-chest">Entire Chest</a></li>
+				</ul>
 			</div>
 		</div>
 
-		<div class="small-12 large-4 columns text-center">
-			<div class="group">
+		<div class="small-12 large-4 columns">
+			<div class="group" data-equalizer-watch>
 				<h3><a href="/legs">Legs</a></h3>
-				<p><a href="/legs/#hamstrings">Hamstrings</a></p>
-				<p><a href="/legs/#quadriceps">Quadriceps</a></p>
-				<p><a href="/legs/#calves">Calves</a></p>
-				<p><a href="/legs/#butt">Butt</a></p>
+				<ul>
+					<li><a href="/legs/#hamstrings">Hamstrings</a></li>
+					<li><a href="/legs/#quadriceps">Quadriceps</a></li>
+					<li><a href="/legs/#calves">Calves</a></li>
+					<li><a href="/legs/#butt">Butt</a></li>
+				</ul>
 			</div>
 		</div>
 
-		<div class="small-12 large-4 columns text-center">
-			<div class="group">
+		<div class="small-12 large-4 columns">
+			<div class="group" data-equalizer-watch>
 				<h3><a href="/shoulders">Shoulders</a></h3>
-				<p><a href="/shoulders/#front-side">Front Side</a></p>
-				<p><a href="/shoulders/#back-side">Back Side</a></p>
-				<p><a href="/shoulders/#entire-shoulder">Entire Shoulder</a></p>
+				<ul>
+					<li><a href="/shoulders/#front-side">Front Side</a></li>
+					<li><a href="/shoulders/#back-side">Back Side</a></li>
+					<li><a href="/shoulders/#entire-shoulder">Entire Shoulder</a></li>
+				</ul>
 			</div>
 		</div>
-
-	</div>
-
-	<div class="row">
-
-		<div class="small-12 large-4 columns text-center">
-			<div class="group">
+		<div class="small-12 large-4 columns">
+			<div class="group" data-equalizer-watch>
 				<h3><a href="/back">Back</a></h3>
-				<p><a href="/back/#upper-back">Upper Back</a></p>
-				<p><a href="/back/#lower-back">Lower Back</a></p>
+				<ul>
+					<li><a href="/back/#upper-back">Upper Back</a></li>
+					<li><a href="/back/#lower-back">Lower Back</a></li>
+				</ul>
 			</div>
 		</div>
 
-		<div class="small-12 large-4 columns text-center">
-			<div class="group">
+		<div class="small-12 large-4 columns">
+			<div class="group" data-equalizer-watch>
 				<h3><a href="/abs">Abs</a></h3>
-				<p><a href="/abs/#upper-abs">Upper Abs</a></p>
-				<p><a href="/abs/#lower-abs">Lower Abs</a></p>
-				<p><a href="/abs/#obliques">Obliques</a></p>
-				<p><a href="/abs/#entire-abs">Entire Abs</a></p>
+				<ul>
+					<li><a href="/abs/#upper-abs">Upper Abs</a></li>
+					<li><a href="/abs/#lower-abs">Lower Abs</a></li>
+					<li><a href="/abs/#obliques">Obliques</a></li>
+					<li><a href="/abs/#entire-abs">Entire Abs</a></li>
+				</ul>
 			</div>
 		</div>
 		
-		<div class="small-12 large-4 columns text-center">
-			<div class="group">
+		<div class="small-12 large-4 columns">
+			<div class="group" data-equalizer-watch>
 				<h3><a href="/arms">Arms</a></h3>
-				<p><a href="/arms/#forearms">Forearms</a></p>
-				<p><a href="/arms/#back-forearm">Back of the Forearms</a></p>
+				<ul>
+					<li><a href="/arms/#forearms">Forearms</a></li>
+					<li><a href="/arms/#back-forearm">Back of the Forearms</a></li>
+				</ul>
 			</div>
 		</div>
-	</div>
-
-	<div class="row">
-		<div class="small-12 large-4 columns text-center">
-			<div class="group">
+		<div class="small-12 large-4 columns">
+			<div class="group" data-equalizer-watch>
 				<h3><a href="/biceps">Biceps</a></h3>
-				<p><a href="/biceps/#entire-bicep">Full Bicep</a></p>
+				<ul>
+					<li><a href="/biceps/#entire-bicep">Full Bicep</a></li>
+				</ul>
 			</div>
 		</div>
 
-		<div class="small-12 large-4 columns text-center">
-			<div class="group">
+		<div class="small-12 large-4 columns end">
+			<div class="group" data-equalizer-watch>
 				<h3><a href="/triceps">Triceps</a></h3>
-				<p><a href="/triceps/#entire-tricep">Full Tricep</a></p>
+				<ul>
+					<li><a href="/triceps/#entire-tricep">Full Tricep</a></li>
+				</ul>
 			</div>
 		</div>
 	</div>
