adac4e129518438d26316ab73b1e44a318080273
Scrivs <scrivs@gmail.com>
2016-10-31 11:59:39 -0700

    init

--- wp-content/themes/thrivestrive/single.php
+++ wp-content/themes/thrivestrive/single.php
@@ -66,7 +66,7 @@ get_header();
 			<h3>Grab the free guide to our Top 10 Favorite Ab Exercises.</h3>
 		</div>
 		<div class="small-12 large-2 columns hide-for-medium-only">
-			<img src="https://s3-us-west-2.amazonaws.com/thrivestrive-master/wp-content/uploads/2016/10/31145618/top-10-abs.png" class="">
+			<img src="https://s3-us-west-2.amazonaws.com/thrivestrive-master/wp-content/uploads/2016/10/31145618/top-10-abs.png" class="" data-pin-nopin="true">
 		</div>
 	</div>
 </section>
