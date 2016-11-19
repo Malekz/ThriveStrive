6dcca08578fc6120c8b4755b6d6b7440c9ee25b3
Scrivs <scrivs@gmail.com>
2016-11-19 10:01:10 -0800

    init

---
+++ wp-content/themes/thrivestrive/partials/nutrition-page.php
@@ -0,0 +1,13 @@
+<div class="callout">
+<h3>Athletic Greens</h3>
+
+You want to get healthy and take all of the right vitamins and supplements, but it's easy to forget. There are just so many!
+
+We recommend you give <a href="https://thrivestrive.com/go/athletic-greens/" class="thirstylink" rel="nofollow" target="_blank" title="Athletic Greens">Athletic Greens</a> a shot.
+
+Athletic Greens combines all of the vitamins and minerals that you need in a single powder. This allows you to save a massive amount of time and money. What makes it really special is that it is diet friendly and completely free of common allergens (eggs, nuts, dairy, gluten, yeast, etc.).
+
+Seriously, it is impossible to list all of the stuff this superpowder contains.
+
+<a href="https://thrivestrive.com/go/athletic-greens/" class="thirstylink button expanded large" rel="nofollow" target="_blank" title="Athletic Greens">Check Out Athletic Greens Today</a>
+</div>
