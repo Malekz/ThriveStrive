6ee07cf4669bb052596a0e9120a09319d4872aa9
Scrivs <scrivs@gmail.com>
2016-11-19 10:55:16 -0800

    init

--- wp-content/themes/thrivestrive/partials/nutrition-page.php
+++ wp-content/themes/thrivestrive/partials/nutrition-page.php
@@ -1,13 +1,13 @@
 <div class="callout">
 <h3>Athletic Greens</h3>
 
-You want to get healthy and take all of the right vitamins and supplements, but it's easy to forget. There are just so many!
+<p>You want to get healthy and take all of the right vitamins and supplements, but it's easy to forget. There are just so many!</p>
 
-We recommend you give <a href="https://thrivestrive.com/go/athletic-greens/" class="thirstylink" rel="nofollow" target="_blank" title="Athletic Greens">Athletic Greens</a> a shot.
+<p>We recommend you give <a href="https://thrivestrive.com/go/athletic-greens/" class="thirstylink" rel="nofollow" target="_blank" title="Athletic Greens">Athletic Greens</a> a shot.</p>
 
-Athletic Greens combines all of the vitamins and minerals that you need in a single powder. This allows you to save a massive amount of time and money. What makes it really special is that it is diet friendly and completely free of common allergens (eggs, nuts, dairy, gluten, yeast, etc.).
+<p>Athletic Greens combines all of the vitamins and minerals that you need in a single powder. This allows you to save a massive amount of time and money. What makes it really special is that it is diet friendly and completely free of common allergens (eggs, nuts, dairy, gluten, yeast, etc.).</p>
 
-Seriously, it is impossible to list all of the stuff this superpowder contains.
+<p>Seriously, it is impossible to list all of the stuff this superpowder contains.</p>
 
-<a href="https://thrivestrive.com/go/athletic-greens/" class="thirstylink button expanded large" rel="nofollow" target="_blank" title="Athletic Greens">Check Out Athletic Greens Today</a>
+<p><a href="https://thrivestrive.com/go/athletic-greens/" class="thirstylink button expanded large" rel="nofollow" target="_blank" title="Athletic Greens">Check Out Athletic Greens Today</a></p>
 </div>
