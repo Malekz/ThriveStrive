aeb4cccbca1062b1ae48bd13932b2883db1ff1da
Scrivs <scrivs@gmail.com>
2016-11-19 10:52:19 -0800

    init

--- wp-content/themes/thrivestrive/functions.php
+++ wp-content/themes/thrivestrive/functions.php
@@ -3,3 +3,8 @@
 //wp_enqueue_style( 'style', get_stylesheet_uri() );
 
 //wp_enqueue_script( 'script', get_template_directory_uri() . '/js/foundation.min.js', array ( 'jquery' ), 2.2, true);
+
+add_action( 'after_setup_theme', 'woocommerce_support' );
+function woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
