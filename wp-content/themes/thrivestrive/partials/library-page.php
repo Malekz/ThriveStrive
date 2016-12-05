1413af3d2e68f8be1eda656857aeec68e5712d8f
Scrivs <scrivs@gmail.com>
2016-12-05 07:54:34 -0800

    init

--- wp-content/themes/thrivestrive/partials/library-page.php
+++ wp-content/themes/thrivestrive/partials/library-page.php
@@ -1,5 +1,5 @@
 <div class="callout">
-<h3>Want Unlimited Access to Our Digital Library?</h3>
+<h3>Want Learn Our Secrets for Losing Weight and Keeping It Off?</h3>
 
 <p>At Thrive/Strive we understand that getting the body that you want is a journey and it doesn&rsquo;t happen overnight.</p>
 
@@ -7,7 +7,7 @@
 
 <p><strong>Gaining access to the Thrive/Strive digital library means that you will have unlimited access to all of our mini-guides forever.</strong> Even better, when a guide is updated you will get sent the newest version.</p>
 
-<p>When have you ever bought a book that received updates for years?</p>
+<p>When have you ever bought a book that received updates for years? <strong>What would you do a with a lifetime subscription to a library that adds 2 new books every single month?</strong></p>
 
 <p>The goal of the Thrive/Strive digital library is to help you become as well-rounded as possible and truly understand what works and what doesn&rsquo;t for your body. As new books are added to the library you will receive them.</p>
 
@@ -27,6 +27,21 @@
 <li>19 Fat-Burning Smoothie Recipes (36 pages)</li>
 <li>16 Zero-Calorie Foods and Awesome Recipes (56 pages)</li>
 </ul>
+
+<div class="row collapse gallery">
+	<div class="small-6 large-3 columns">
+		<div class="image"><img src="https://s3-us-west-2.amazonaws.com/thrivestrive-master/wp-content/uploads/2016/12/05154443/fat-burning-smoothies.png" data-pin-nopin="true"></div>
+	</div>
+	<div class="small-6 large-3 columns">
+		<div class="image"><img src="https://s3-us-west-2.amazonaws.com/thrivestrive-master/wp-content/uploads/2016/12/05154444/lazy-ways.png" data-pin-nopin="true"></div>
+	</div>
+	<div class="small-6 large-3 columns">
+		<div class="image"><img src="https://s3-us-west-2.amazonaws.com/thrivestrive-master/wp-content/uploads/2016/12/05154447/low-carb-veggies.png" data-pin-nopin="true"></div>
+	</div>
+	<div class="small-6 large-3 columns">
+		<div class="image"><img src="https://s3-us-west-2.amazonaws.com/thrivestrive-master/wp-content/uploads/2016/12/05154449/zero-calorie-foods.png" data-pin-nopin="true"></div>
+	</div>
+</div>
 
 <p>More guides will be added each month and you will always have access to them. We want the world to be happy and that starts with loving who you see in the mirror.</p>
 
