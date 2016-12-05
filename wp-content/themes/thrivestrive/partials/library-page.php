d57b40906b8b1f6e9de92cede2e2a3a17bf7a3ac
Scrivs <scrivs@gmail.com>
2016-12-05 00:43:03 -0800

    init

---
+++ wp-content/themes/thrivestrive/partials/library-page.php
@@ -0,0 +1,36 @@
+<div class="callout">
+<h3>Want Unlimited Access to Our Digital Library?</h3>
+
+<p>At Thrive/Strive we understand that getting the body that you want is a journey and it doesn&rsquo;t happen overnight.</p>
+
+<p>Every day there are new advances being discovered in the health &amp; wellness community and it can be a real pain trying to keep up with it all. That is why we do it for you.</p>
+
+<p>Gaining access to the Thrive/Strive digital library means that you will have unlimited access to all of our mini-guides forever. Even better, when a guide is updated you will get sent the newest version.</p>
+
+<p>When have you ever bought a book that received updates for years?</p>
+
+<p>The goal of the Thrive/Strive digital library is to help you become as well-rounded as possible and truly understand what works and what doesn&rsquo;t for your body. As new books are added to the library you will receive them.</p>
+
+<p>Once you purchase access to the library you have it forever. No tricks.</p>
+
+<p>Are you ready to get the body you want and keep it?</p>
+
+<p class="text-center"><a data-dpd-type="button" data-variant="price-right" data-text="PURCHASE ACCESS NOW" data-button-size="dpd-large" data-bg-color="ff8800" data-bg-color-hover="ffa10b" data-text-color="ffffff" data-pr-bg-color="ffffff" data-pr-color="000000" data-lightbox="1" href="https://thrive-strive.dpdcart.com/cart/add?product_id=141860&amp;method_id=151567">PURCHASE ACCESS NOW</a><script src="https://thrive-strive.dpdcart.com/dpd.js"></script></p>
+
+<h4>Current Titles</h4>
+
+<p>The current titles in the digital library include:</p>
+
+<ul>
+<li>23 Lazy Ways to Lose Weight</li>
+<li>31 Low-Carb Veggie Recipes</li>
+<li>19 Fat-Burning Smoothie Recipes</li>
+<li>16 Zero-Calorie Foods and Awesome Recipes</li>
+</ul>
+
+<p>More guides will be added each month and you will always have access to them. We want the world to be happy and that starts with loving who you see in the mirror.</p>
+
+<p>Gain lifetime access to the Thrive/Strive digital library for just $13.</p>
+
+<p>If you don&rsquo;t start now, when will you finally begin?</p>
+</div>
