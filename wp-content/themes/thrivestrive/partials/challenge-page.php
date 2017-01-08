928f7f5362fd204c41077fcb6c67848c7f665967
Scrivs <scrivs@gmail.com>
2017-01-07 20:53:41 -0800

    init

--- wp-content/themes/thrivestrive/partials/challenge-page.php
+++ wp-content/themes/thrivestrive/partials/challenge-page.php
@@ -2,14 +2,14 @@
 	<h2 class="text-center" style="color: #FFF;margin-bottom: 2rem;">Hard to Be Happy Without Being Healthy. It&rsquo;s Time to Challenge Yourself to Be Better.</h2>
 </div>
 <div class="small-6 large-3 columns">
-	<div class="nolink"><a href="/muffin-top-challenge"><img src="https://s3-us-west-2.amazonaws.com/thrivestrive-master/wp-content/uploads/2016/12/31223117/muffin-top.jpg" data-pin-nopin="true"></a></div>
-</div>
-<div class="small-6 large-3 columns">
-	<div class="nolink"><a href="/booty-builder-challenge"><img src="https://s3-us-west-2.amazonaws.com/thrivestrive-master/wp-content/uploads/2016/12/31223113/booty-builder.jpg" data-pin-nopin="true"></a></div>
-</div>
-<div class="small-6 large-3 columns">
-	<div class="nolink"><a href="/thunder-thighs-challenge"><img src="https://s3-us-west-2.amazonaws.com/thrivestrive-master/wp-content/uploads/2016/12/31223122/thunder-thighs.jpg" data-pin-nopin="true"></a></div>
-</div>
-<div class="small-6 large-3 columns">
-	<div class="nolink"><a href="/body-blaster-challenge"><img src="https://s3-us-west-2.amazonaws.com/thrivestrive-master/wp-content/uploads/2016/12/31223109/body-blaster.jpg" data-pin-nopin="true"></a></div>
-</div>
+						<div class="nolink"><a href="/muffin-top-challenge"><img src="https://s3-us-west-2.amazonaws.com/thrivestrive-master/wp-content/uploads/2017/01/08044413/muffin-top.jpg" data-pin-nopin="true"></a></div>
+					</div>
+					<div class="small-6 large-3 columns">
+						<div class="nolink"><a href="/booty-builder-challenge"><img src="https://s3-us-west-2.amazonaws.com/thrivestrive-master/wp-content/uploads/2017/01/08044411/booty-builder.jpg" data-pin-nopin="true"></a></div>
+					</div>
+					<div class="small-6 large-3 columns">
+						<div class="nolink"><a href="/thunder-thighs-challenge"><img src="https://s3-us-west-2.amazonaws.com/thrivestrive-master/wp-content/uploads/2017/01/08044416/thunder-thighs.jpg" data-pin-nopin="true"></a></div>
+					</div>
+					<div class="small-6 large-3 columns">
+						<div class="nolink"><a href="/body-blaster-challenge"><img src="https://s3-us-west-2.amazonaws.com/thrivestrive-master/wp-content/uploads/2017/01/08044410/body-blaster.jpg" data-pin-nopin="true"></a></div>
+					</div>
