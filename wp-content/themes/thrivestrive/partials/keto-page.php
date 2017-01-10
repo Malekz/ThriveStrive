2f3a3405d2f436498e9f3f39c7d867dfba949e5e
Scrivs <scrivs@gmail.com>
2017-01-10 14:20:53 -0800

    init

--- wp-content/themes/thrivestrive/partials/keto-page.php
+++ wp-content/themes/thrivestrive/partials/keto-page.php
@@ -1,10 +1,10 @@
 <div class="callout">
 <h3>Ready to Lose 10-21 lbs in the Next 21 Days?</h3>
-<p>If you're really serious about losing weight and want to create a new lifestyle for yourself then the <a href="/keto-dash" target="_blank" rel="nofollow">Keto Dash System</a> is for you.</p>
+<p>If you're really serious about losing weight and want to create a new lifestyle for yourself then the <a href="https://club.ketodash.com" target="_blank" rel="nofollow">Keto Dash System</a> is for you.</p>
 
 <p>What if you were told that doctors and scientists have been using a diet for over 50 years to help cure obesity, diabetes, heart disease, high cholesterol, high blood pressure, cardiovascular disease, bipolar disease, and even cancer?</p>
 
-<p><a href="/keto-dash" target="_blank" rel="nofollow">A diet</a> that not only helps you lose weight, but also offers these health benefits:</p>
+<p><a href="https://club.ketodash.com" target="_blank" rel="nofollow">A diet</a> that not only helps you lose weight, but also offers these health benefits:</p>
 
 <ul>
 	<li>Freedom from hypoglycemia, food fixations, and sugar cravings</li>
@@ -23,7 +23,7 @@
 	<li>Mood stabilization</li>
 </ul>
 
-<p>Sounds too good to be true, right? Well it isn't and I am happy to teach you all about it with the <a href="/keto-dash" target="_blank" rel="nofollow">Keto Dash System</a>.</p>
+<p>Sounds too good to be true, right? Well it isn't and I am happy to teach you all about it with the <a href="https://club.ketodash.com" target="_blank" rel="nofollow">Keto Dash System</a>.</p>
 
-<p><a href="/keto-dash" class="thirstylink button expanded large" rel="nofollow" target="_blank" title="Keto Dash: Season 1">Check Out The Keto Dash System Now</a></p>
+<p><a href="https://club.ketodash.com" class="thirstylink button expanded large" rel="nofollow" target="_blank" title="Keto Dash: Season 1">Check Out 28-Day Weight Loss Challenge Now</a></p>
 </div>
