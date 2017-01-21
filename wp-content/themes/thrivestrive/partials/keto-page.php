b0e36363e234cb52ae8f129fc731afd325056391
Scrivs <scrivs@gmail.com>
2017-01-21 08:12:52 -0800

    init

--- wp-content/themes/thrivestrive/partials/keto-page.php
+++ wp-content/themes/thrivestrive/partials/keto-page.php
@@ -2,28 +2,13 @@
 <h3>Ready to Lose 10-21 lbs in the Next 28 Days?</h3>
 <p>If you're really serious about losing weight and want to create a new lifestyle for yourself then the <a href="https://club.ketodash.com" target="_blank" rel="nofollow">28-Day Weight Loss Challenge</a> is for you.</p>
 
-<p>What if you were told that doctors and scientists have been using a diet for over 50 years to help cure obesity, diabetes, heart disease, high cholesterol, high blood pressure, cardiovascular disease, bipolar disease, and even cancer?</p>
+<p>The Ketogenic Diet has changed our lives. It has turned our bodies from sugar-burners to fat-burners.</p>
 
-<p><a href="https://club.ketodash.com" target="_blank" rel="nofollow">A diet</a> that not only helps you lose weight, but also offers these health benefits:</p>
+<p>We wake up with more energy, less cravings, and a better mood throughout the day.</p>
 
-<ul>
-	<li>Freedom from hypoglycemia, food fixations, and sugar cravings</li>
-	<li>Lack of hunger</li>
-	<li>Lower blood pressure</li>
-	<li>Drop in cholesterol</li>
-	<li>Increase in HDL Cholesterol (this is a good thing)</li>
-	<li>A drop in triglycerides</li>
-	<li>More energy</li>
-	<li>Decrease in stiffness and joint pain</li>
-	<li>Clearer thinking</li>
-	<li>Improvement in sleep apnea symptoms</li>
-	<li>Heartburn relief</li>
-	<li>Prevention of gum dieseas</li>
-	<li>Improved digestion</li>
-	<li>Mood stabilization</li>
-</ul>
+<p>Our <a href="https://club.ketodash.com" target="_blank" rel="nofollow">28-Day Weight Loss Challenge</a> walks you through how to incorporate the Ketogenic Diet into your lifestyle so you no longer have to worry about giving up on diets and gaining back the weight you keep on trying to lose.</p>
 
-<p>Sounds too good to be true, right? Well it isn't and I am happy to teach you all about it with the <a href="https://club.ketodash.com" target="_blank" rel="nofollow">28-Day Weight Loss Challenge</a>.</p>
+<p>Sounds too good to be true, right? Well it isn't and we are happy to teach you all about it with the <a href="https://club.ketodash.com" target="_blank" rel="nofollow">28-Day Weight Loss Challenge</a>.</p>
 
 <p><a href="https://club.ketodash.com" class="thirstylink button expanded large" rel="nofollow" target="_blank" title="Keto Dash: Season 1">Check Out the 28-Day Weight Loss Challenge Now</a></p>
 </div>
