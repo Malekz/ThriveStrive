4b06f907a85e74b51a482b892bab49613fd1cb31
Scrivs <scrivs@gmail.com>
2017-06-10 08:58:03 -0700

    init

--- wp-content/themes/thrivestrive/template-pinterest.php
+++ wp-content/themes/thrivestrive/template-pinterest.php
@@ -21,21 +21,11 @@ Template Name: Pinterest Gallery
 				<?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
 					<?php $pinterest = get_field('pinterest'); ?>
 					<?php $pinterest2 = get_field('pinterest_2'); ?>
-					<?php $pinterest3 = get_field('pinterest_3'); ?>
-					<?php $pinterest4 = get_field('pinterest_4'); ?>
-					<?php $pinterest5 = get_field('pinterest_5'); ?>
-					<?php $pinterest6 = get_field('pinterest_6'); ?>
-					<?php $pinterest7 = get_field('pinterest_7'); ?>
-					<?php $pinterest8 = get_field('pinterest_8'); ?>
-					<?php $pinterest9 = get_field('pinterest_9'); ?>
-					<?php $pinterest10 = get_field('pinterest_10'); ?>
-					<?php $pinterest11 = get_field('pinterest_11'); ?>
-					<?php $pinterest12 = get_field('pinterest_12'); ?>
-					<?php $pinterest13 = get_field('pinterest_13'); ?>
-					<?php $pinterest14 = get_field('pinterest_14'); ?>
-					<?php $pinterest15 = get_field('pinterest_15'); ?>
 					<?php $pinterest_description = get_field('pinterest_description'); ?>
 					<li><a href="<?php the_permalink(); ?>"><img alt="<?php the_title();?>" src="<?php echo $pinterest;?>" data-pin-url="<?php the_permalink(); ?>" data-pin-description="<?php echo $pinterest_description; ?>"></a></li>
+					<?php if(! empty($pinterest2)) { ?>
+						<li><a href="<?php the_permalink(); ?>"><img alt="<?php the_title();?>" src="<?php echo $pinterest2;?>" data-pin-url="<?php the_permalink(); ?>" data-pin-description="<?php echo $pinterest_description; ?>"></a></li>
+					<?php } ?>
 				<?php endwhile; ?>
 				<!-- end of the loop -->
 
