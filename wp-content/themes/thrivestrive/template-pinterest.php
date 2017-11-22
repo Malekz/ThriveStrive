db96b28faa9fb91ce1d229b344b0f2f4fe750498
Scrivs <scrivs@gmail.com>
2017-11-22 09:42:03 -0800

    init

--- wp-content/themes/thrivestrive/template-pinterest.php
+++ wp-content/themes/thrivestrive/template-pinterest.php
@@ -24,24 +24,29 @@ Template Name: Pinterest Gallery
 						$pinterest5 = get_field('pinterest_5');
 						$pinterest6 = get_field('pinterest_6');
 						$pinterest_description = get_field('pinterest_description');
+						$pinterest_description_2 = get_field('pinterest_description_2');
+						$pinterest_description_3 = get_field('pinterest_description_3');
+						$pinterest_description_4 = get_field('pinterest_description_4');
+						$pinterest_description_5 = get_field('pinterest_description_5');
+						$pinterest_description_6 = get_field('pinterest_description_6');
 					?>
 					<?php if(! empty($pinterest)) { ?>
-						<div class="small-12 large-2 columns" style="margin-bottom: 1rem;"><a href="<?php the_permalink(); ?>"><img alt="<?php the_title();?>" src="<?php echo $pinterest;?>" data-pin-url="<?php the_permalink(); ?>" data-pin-description="<?php echo $pinterest_description; ?>"></a></div>
+						<div class="small-12 large-2 columns" style="margin-bottom: 1rem;"><a href="<?php the_permalink(); ?>"><img alt="<?php echo $pinterest_description; ?>" src="<?php echo $pinterest;?>" data-pin-url="<?php the_permalink(); ?>" data-pin-description="<?php echo $pinterest_description; ?>"></a></div>
 					<?php } ?>
 					<?php if(! empty($pinterest2)) { ?>
-						<div class="small-12 large-2 columns" style="margin-bottom: 1rem;"><a href="<?php the_permalink(); ?>"><img alt="<?php the_title();?>" src="<?php echo $pinterest2;?>" data-pin-url="<?php the_permalink(); ?>" data-pin-description="<?php echo $pinterest_description; ?>"></a></div>
+						<div class="small-12 large-2 columns" style="margin-bottom: 1rem;"><a href="<?php the_permalink(); ?>"><img alt="<?php echo $pinterest_description_2; ?>" src="<?php echo $pinterest2;?>" data-pin-url="<?php the_permalink(); ?>" data-pin-description="<?php echo $pinterest_description_2; ?>"></a></div>
 					<?php } ?>
 					<?php if(! empty($pinterest3)) { ?>
-						<div class="small-12 large-2 columns" style="margin-bottom: 1rem;"><a href="<?php the_permalink(); ?>"><img alt="<?php the_title();?>" src="<?php echo $pinterest3;?>" data-pin-url="<?php the_permalink(); ?>" data-pin-description="<?php echo $pinterest_description; ?>"></a></div>
+						<div class="small-12 large-2 columns" style="margin-bottom: 1rem;"><a href="<?php the_permalink(); ?>"><img alt="<?php echo $pinterest_description_3; ?>" src="<?php echo $pinterest3;?>" data-pin-url="<?php the_permalink(); ?>" data-pin-description="<?php echo $pinterest_description_3; ?>"></a></div>
 					<?php } ?>
 					<?php if(! empty($pinterest4)) { ?>
-						<div class="small-12 large-2 columns" style="margin-bottom: 1rem;"><a href="<?php the_permalink(); ?>"><img alt="<?php the_title();?>" src="<?php echo $pinterest4;?>" data-pin-url="<?php the_permalink(); ?>" data-pin-description="<?php echo $pinterest_description; ?>"></a></div>
+						<div class="small-12 large-2 columns" style="margin-bottom: 1rem;"><a href="<?php the_permalink(); ?>"><img alt="<?php echo $pinterest_description_4; ?>" src="<?php echo $pinterest4;?>" data-pin-url="<?php the_permalink(); ?>" data-pin-description="<?php echo $pinterest_description_4; ?>"></a></div>
 					<?php } ?>
 					<?php if(! empty($pinterest5)) { ?>
-						<div class="small-12 large-2 columns" style="margin-bottom: 1rem;"><a href="<?php the_permalink(); ?>"><img alt="<?php the_title();?>" src="<?php echo $pinterest5;?>" data-pin-url="<?php the_permalink(); ?>" data-pin-description="<?php echo $pinterest_description; ?>"></a></div>
+						<div class="small-12 large-2 columns" style="margin-bottom: 1rem;"><a href="<?php the_permalink(); ?>"><img alt="<?php echo $pinterest_description_5; ?>" src="<?php echo $pinterest5;?>" data-pin-url="<?php the_permalink(); ?>" data-pin-description="<?php echo $pinterest_description_5; ?>"></a></div>
 					<?php } ?>
 					<?php if(! empty($pinterest6)) { ?>
-						<div class="small-12 large-2 columns" style="margin-bottom: 1rem;"><a href="<?php the_permalink(); ?>"><img alt="<?php the_title();?>" src="<?php echo $pinterest6;?>" data-pin-url="<?php the_permalink(); ?>" data-pin-description="<?php echo $pinterest_description; ?>"></a></div>
+						<div class="small-12 large-2 columns" style="margin-bottom: 1rem;"><a href="<?php the_permalink(); ?>"><img alt="<?php echo $pinterest_description_6; ?>" src="<?php echo $pinterest6;?>" data-pin-url="<?php the_permalink(); ?>" data-pin-description="<?php echo $pinterest_description_6; ?>"></a></div>
 					<?php } ?>
 				<?php endwhile; ?>
 				<!-- end of the loop -->
