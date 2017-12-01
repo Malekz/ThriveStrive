dcb656ae7eb97e13b29af0110ca3ea412842fe8e
Scrivs <scrivs@gmail.com>
2017-11-30 20:06:20 -0800

    init

--- wp-content/themes/thrivestrive/single.php
+++ wp-content/themes/thrivestrive/single.php
@@ -24,6 +24,36 @@ get_header();
 	<div class="row">
 		<div class="small-12 large-8 columns post">
 			<?php the_content(); ?>
+		</div>
+		<div class="small-12 large-4 columns hide-for-small-only">
+			<div class="sidebar">
+				<div class="image">
+					<img src="<?php echo $pinterest;?>" data-pin-url="<?php the_permalink(); ?>" data-pin-description="<?php echo $pinterest_description; ?>">
+				</div>
+				<h3>Welcome to Thrive/Strive!</h3>
+				<p>We are the Thrive/Strive crew. A family just trying to find the best ways to achieve happier bodies.</p>
+				<p>On Thrive/Strive we share our ups and downs, experiments, and overall journey so hopefully inspire and educate other awesome people.</p>
+				<p>We are so happy you are here! If you are looking to get a healthier body and happier state of mind then this is the place to be.</p>
+				<div class="image">
+					<img src="https://s3-us-west-2.amazonaws.com/thrivestrive-master/wp-content/uploads/2016/08/02222125/tscrew.jpg" data-pin-nopin="true" nopin alt="Thrive/Strive Crew">
+				</div>
+				<h3>Challenge Yourself</h3>
+				<div class="image">
+					<!--<script src="//static.leadpages.net/leadboxes/current/embed.js" async defer></script> <a href="" style="margin-bottom: 0;" class="" data-leadbox-popup="1454bc973f72a2:16408842c946dc" data-leadbox-domain="billionaireblogclub.lpusercontent.com"><img data-pin-description="Transform your body in 3 days and change your life forever with the 3-day weight loss challenge." data-pin-url="https://ketodash.com/weight-loss-challenge" data-pin-media="https://s3-us-west-2.amazonaws.com/bitchafit-master/wp-content/uploads/2017/08/23200626/weight-loss-challenge-pin3.jpg" data-pin-custom="true" data-pin-do="buttonPin" alt="Transform your body in 3 days and change your life forever with the 3-day weight loss challenge." src="https://s3-us-west-2.amazonaws.com/bitchafit-master/wp-content/uploads/2017/08/23200626/weight-loss-challenge-pin3.jpg"></a>-->
+					<a href="https://ketodash.com/join?utm_source=thrivestrive_post_footer&utm_campaign=<?php echo $post_slug; ?>&utm_medium=blog_post"><img src="https://s3-us-west-2.amazonaws.com/ketodash-master/blog/wp-content/uploads/2017/10/09041009/weight-loss-challenge-pin2.jpg" data-pin-norepin="true"></a>
+				</div>
+				<h3>Get Started With Keto</h3>
+				<p>Our book, <em><a href="https://ketodash.com/weight-loss-manual">The 3-Day Weight Loss Manual</a></em>, will help to quickly get you up to speed on the Ketogenic Diet along with practical advice on how to achieve ketosis and weight loss as quickly as possible.</p>
+				<div class="image">
+					<a href="https://ketodash.com/weight-loss-manual"><img src="https://s3-us-west-2.amazonaws.com/bitchafit-master/wp-content/uploads/2017/08/24194240/weight-loss-manual.jpg" data-pin-nopin="true"></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="article">
+	<div class="row">
+		<div class="small-12 columns">
 			<?php 
 				get_template_part('partials/keto', 'page');
 				// $posttags = get_the_tags();
@@ -55,29 +85,6 @@ get_header();
 				//  	get_template_part('partials/weightloss', 'page');
 				// }
 			?>
-		</div>
-		<div class="small-12 large-4 columns hide-for-small-only">
-			<div class="sidebar">
-				<div class="image">
-					<img src="<?php echo $pinterest;?>" data-pin-url="<?php the_permalink(); ?>" data-pin-description="<?php echo $pinterest_description; ?>">
-				</div>
-				<h3>Welcome to Thrive/Strive!</h3>
-				<p>We are the Thrive/Strive crew. A family just trying to find the best ways to achieve happier bodies.</p>
-				<p>On Thrive/Strive we share our ups and downs, experiments, and overall journey so hopefully inspire and educate other awesome people.</p>
-				<p>We are so happy you are here! If you are looking to get a healthier body and happier state of mind then this is the place to be.</p>
-				<div class="image">
-					<img src="https://s3-us-west-2.amazonaws.com/thrivestrive-master/wp-content/uploads/2016/08/02222125/tscrew.jpg" data-pin-nopin="true" nopin alt="Thrive/Strive Crew">
-				</div>
-				<h3>Challenge Yourself</h3>
-				<div class="image">
-					<script src="//static.leadpages.net/leadboxes/current/embed.js" async defer></script> <a href="" style="margin-bottom: 0;" class="" data-leadbox-popup="1454bc973f72a2:16408842c946dc" data-leadbox-domain="billionaireblogclub.lpusercontent.com"><img data-pin-description="Transform your body in 3 days and change your life forever with the 3-day weight loss challenge." data-pin-url="https://ketodash.com/weight-loss-challenge" data-pin-media="https://s3-us-west-2.amazonaws.com/bitchafit-master/wp-content/uploads/2017/08/23200626/weight-loss-challenge-pin3.jpg" data-pin-custom="true" data-pin-do="buttonPin" alt="Transform your body in 3 days and change your life forever with the 3-day weight loss challenge." src="https://s3-us-west-2.amazonaws.com/bitchafit-master/wp-content/uploads/2017/08/23200626/weight-loss-challenge-pin3.jpg"></a>
-				</div>
-				<h3>Get Started With Keto</h3>
-				<p>Our book, <em><a href="https://ketodash.com/weight-loss-manual">The 3-Day Weight Loss Manual</a></em>, will help to quickly get you up to speed on the Ketogenic Diet along with practical advice on how to achieve ketosis and weight loss as quickly as possible.</p>
-				<div class="image">
-					<a href="https://ketodash.com/weight-loss-manual"><img src="https://s3-us-west-2.amazonaws.com/bitchafit-master/wp-content/uploads/2017/08/24194240/weight-loss-manual.jpg" data-pin-nopin="true"></a>
-				</div>
-			</div>
 		</div>
 	</div>
 </section>
