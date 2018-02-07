6262385b62d2ce2c54bf4a77114aa13113cef435
Paul Scrivens <scrivs@Pauls-MacBook-Pro.local>
2018-02-06 20:25:12 -0800

    init

--- wp-content/themes/thrivestrive/partials/keto-page.php
+++ wp-content/themes/thrivestrive/partials/keto-page.php
@@ -3,21 +3,21 @@
 <h2>Ready to Lose 10-21 lbs in the Next 28 Days?</h2>
 <div class="row">
 	<div class="small-12 large-8 columns">
-		<p>If you're really serious about losing weight and want to create a new lifestyle for yourself then the <a href="https://ketodash.com/join?utm_source=thrivestrive_post_footer&utm_campaign=<?php echo $post_slug; ?>&utm_medium=blog_post" target="_blank" rel="nofollow">28-Day Weight Loss Challenge</a> is for you.</p>
+		<p>If you're really serious about losing weight and want to create a new lifestyle for yourself then the <a href="https://ketodash.com/join?rfsn=1032346.edd7e&utm_source=thrivestrive_post_footer&utm_campaign=<?php echo $post_slug; ?>&utm_medium=blog_post" target="_blank" rel="nofollow">28-Day Weight Loss Challenge</a> is for you.</p>
 
 		<p>The Ketogenic Diet has changed our lives. It has turned our bodies from sugar-burners to fat-burners.</p>
 
 		<p>We wake up with more energy, less cravings, and a better mood throughout the day.</p>
 
-		<p>Our <a href="https://ketodash.com/join?utm_source=thrivestrive_post_footer&utm_campaign=<?php echo $post_slug; ?>&utm_medium=blog_post" target="_blank" rel="nofollow">28-Day Weight Loss Challenge</a> walks you through how to incorporate the Ketogenic Diet into your lifestyle so you no longer have to worry about giving up on diets and gaining back the weight you keep on trying to lose.</p>
+		<p>Our <a href="https://ketodash.com/join?rfsn=1032346.edd7e&utm_source=thrivestrive_post_footer&utm_campaign=<?php echo $post_slug; ?>&utm_medium=blog_post" target="_blank" rel="nofollow">28-Day Weight Loss Challenge</a> walks you through how to incorporate the Ketogenic Diet into your lifestyle so you no longer have to worry about giving up on diets and gaining back the weight you keep on trying to lose.</p>
 
-		<p>Sounds too good to be true, right? Well it isn't and we are happy to teach you all about it with the <a href="https://ketodash.com/join?utm_source=thrivestrive_post_footer&utm_campaign=<?php echo $post_slug; ?>&utm_medium=blog_post" target="_blank" rel="nofollow">28-Day Weight Loss Challenge</a>.</p>
+		<p>Sounds too good to be true, right? Well it isn't and we are happy to teach you all about it with the <a href="https://ketodash.com/join?rfsn=1032346.edd7e&utm_source=thrivestrive_post_footer&utm_campaign=<?php echo $post_slug; ?>&utm_medium=blog_post" target="_blank" rel="nofollow">28-Day Weight Loss Challenge</a>.</p>
 		<div class="image"><img src="https://s3-us-west-2.amazonaws.com/bitchafit-master/wp-content/uploads/2017/09/07220718/testimonial31.jpg" data-pin-norepin="true"></div>
 		<div class="image"><img src="https://s3-us-west-2.amazonaws.com/bitchafit-master/wp-content/uploads/2017/09/07220817/testimonial12.jpg" data-pin-norepin="true"></div>
-		<p><a href="https://ketodash.com/join?utm_source=thrivestrive_post_footer&utm_campaign=<?php echo $post_slug; ?>&utm_medium=blog_post" target="_blank" rel="nofollow" class="button large expanded">Check Out the 28-Day Weight Loss Challenge Now</a></p>
+		<p><a href="https://ketodash.com/join?rfsn=1032346.edd7e&utm_source=thrivestrive_post_footer&utm_campaign=<?php echo $post_slug; ?>&utm_medium=blog_post" target="_blank" rel="nofollow" class="button large expanded">Check Out the 28-Day Weight Loss Challenge Now</a></p>
 	</div>
 	<div class="small-12 large-4 columns">
-		<a href="https://ketodash.com/join?utm_source=thrivestrive_post_footer&utm_campaign=<?php echo $post_slug; ?>&utm_medium=blog_post"><img src="https://s3-us-west-2.amazonaws.com/ketodash-master/blog/wp-content/uploads/2017/10/09041009/weight-loss-challenge-pin2.jpg" data-pin-norepin="true"></a>
+		<a href="https://ketodash.com/join?rfsn=1032346.edd7e&utm_source=thrivestrive_post_footer&utm_campaign=<?php echo $post_slug; ?>&utm_medium=blog_post"><img src="https://s3-us-west-2.amazonaws.com/ketodash-master/blog/wp-content/uploads/2017/10/09041009/weight-loss-challenge-pin2.jpg" data-pin-norepin="true"></a>
 	</div>
 </div>
 </div>
